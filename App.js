b26f5d5d8638689d9700bf8cb0a2ca213cbefa05
Sven Goller <svengoller1@gmail.com>
2022-06-25 05:48:23 +0200

    WIP: Now plays voice messages and shows progress TODO: skip and rewind with draggable animation

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, FlatList, TextInput, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, FlatList, TextInput, SafeAreaView, Animated, Easing, PanResponder} from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 import { mockup_messages } from './Messages';
+import { Audio } from 'expo-av';
+import React, { useEffect, useState, useRef } from 'react';
+import { audio_mode } from './AudioConfigs';
+
+/******************** LOGIC  ******************/
+Audio.setAudioModeAsync(audio_mode)
+
+/********************* UI *********************/
 
 export default function App() {
   return (
@@ -84,11 +92,120 @@ const Message = (props) => {
  * VoiceMessage Component 
  */
 const VoiceMessage = (props) => {
-  const message = props.message 
+  const message = props.message
+  const [sound, setSound] = useState()
+  const [isPlaying, setIsPlaying] = useState(false)
+  const [isFinishedPlaying, setIsFinishedPlaying] = useState(false)
+  const [progressBarWidth, setProgressBarWidth] = useState(-1)
+
+  const progressAnim = useRef(new Animated.Value(0)).current;  // also used for pan?
+
+  const panResponder = useRef(
+    PanResponder.create({
+      onMoveShouldSetPanResponder: () => true,
+      onPanResponderGrant: () => {
+        progressAnim.setOffset(progressAnim._value);
+      },
+      onPanResponderMove: Animated.event(
+        [
+          null,
+          {dx: progressAnim}
+        ]
+      ),
+      onPanResponderRelease: () => {
+        progressAnim.flattenOffset();
+      }
+    })
+  ).current;
+
+
+  const start_animation = (current, duration) => {
+    Animated.timing(progressAnim, {
+      toValue: progressBarWidth,
+      duration: duration - current,
+      easing: Easing.linear
+    }).start()
+  }
+
+  const stop_animation = () => {
+    progressAnim.stopAnimation();
+  }
+
+  const reset_animation = () => {
+    Animated.timing(progressAnim, {
+      toValue: 0,
+      duration: 0,
+      easing: Easing.linear
+    }).start()
+  }
+  
+  const source = message.audio
+  const initialStatus = {
+    progressUpdateIntervalMillis: 1000,
+    positionMillis: 0,  // TODO: change if it should start at another place
+    rate: 1, 
+  }
+  const onPlaybackStatusUpdate = (status) => {
+    // TODO: maybe animations? Stopping if it is after a certain point (cutting)
+
+    setIsPlaying(status.isPlaying)
+    if (status.didJustFinish) {
+      setIsFinishedPlaying(true)
+      reset_animation()
+    }
+    if (status.isPlaying && isFinishedPlaying) setIsFinishedPlaying(false)
+  }
+
+  useEffect(() => {
+    console.log(isFinishedPlaying)
+  }, [isFinishedPlaying])
+  
+  useEffect(() => {
+    const load_sound = async () => {
+      const { sound } = await Audio.Sound.createAsync(
+        source,
+        initialStatus,
+        onPlaybackStatusUpdate,
+        //downloadFirst  // defaults to true
+      )
+      setSound(sound)
+    }
+
+    load_sound()
+  }, [])
+
+  async function play() {
+    if (isFinishedPlaying) {
+      await sound.replayAsync()
+    } else {
+      await sound.playAsync()
+    }
+    const {positionMillis, durationMillis } = await sound.getStatusAsync()
+    start_animation(positionMillis, durationMillis)
+  }
+
+  async function pause() {
+    stop_animation()
+    await sound.pauseAsync()
+  }
 
   return (
     <View style = {styles.voiceMessage}>
-      <Icon name = {'play-arrow'} size = {30} style = {styles.icon}/>
+      {
+        isPlaying ?
+        <Icon name = {'pause'} size = {30} style = {styles.icon} onPress={pause}/>
+        : 
+        <Icon name = {'play-arrow'} size = {30} style = {styles.icon} onPress={play}/>
+      }
+      <View style = {{flex: 1, height: 30, borderWidth: 0.5, justifyContent: 'center'}}
+        onLayout = {({nativeEvent}) => {setProgressBarWidth(nativeEvent.layout.width)}}>
+        <Animated.View 
+          style = {{width: 25, height: 25, borderRadius: 25, backgroundColor: 'red', transform:[{translateX: progressAnim}]}}
+          {...panResponder.panHandlers}
+        >
+
+        </Animated.View>
+      </View>
     </View>
   )
 }
