15c37ac9d90cddcb8130bc355a11386e65b982d7
Philipp Jahn <philippjahn99@gmx.de>
2022-06-29 16:13:03 +0200

    added flask stuff to backend

--- backend/backend.py
+++ backend/backend.py
@@ -1,3 +1,4 @@
+import json
 import requests
 import re
 import shelve
@@ -6,14 +7,33 @@ import requests
 import os
 import shelve
 from google.cloud import speech_v1 as speech
-
+from flask import Flask,request
+from flask_cors import CORS, cross_origin
 
 # Google Authentication/API
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = ***REMOVED***
 api_key = ***REMOVED***  # Ai21 Api Key
 
 
-def summarize(text):
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+
+######### api #################################################################
+
+@app.route('/test_func', methods = ['POST'])
+@cross_origin()
+def test_func():
+    text = request.get_json(force = True)['text']
+    return json.dumps({"text":"helllllo"})
+
+
+@app.route('/summarize', methods = ['POST'])
+@cross_origin()
+def summarize():
+    text = request.get_json(force = True)['text']
     db = shelve.open("ai21_generic_replies_storage.db")
     with  open("summarization_template.txt", "r") as f1:
         yn_template= f1.read()
@@ -62,9 +82,11 @@ def summarize(text):
     db.close()
     data = response.json()
     res_text = data['completions'][0]['data']['text']
-    return res_text
+    return json.dumps({"original":text, "summarization":res_text})
 
 
+@app.route('/transcribe', methods = ['POST'])
+@cross_origin()
 def speech_to_text_local_audio(config, audio):
     db = shelve.open("speech_to_text_replies_storage.db")
     if not (audio in db):
@@ -86,9 +108,9 @@ def speech_to_text_local_audio(config, audio):
             list.append(
                 {"word": word.word, "confidence": word.confidence, "time": word.start_time})
 
-    return {"transcription": transcription, "words": list}
+    return json.dumps({"transcription": transcription, "words": list})
 
-
+"""
 config_wav = speech.RecognitionConfig(sample_rate_hertz=48000,
                                       enable_automatic_punctuation=True,
                                       language_code='en-US',
@@ -100,3 +122,4 @@ config_wav = speech.RecognitionConfig(sample_rate_hertz=48000,
 response = speech_to_text_local_audio(config_wav, "data/shortaudio.wav")
 print("audio transcription : " + response["transcription"])
 print("summary: " + summarize(response["transcription"]))
+"""
